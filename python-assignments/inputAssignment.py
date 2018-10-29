d232bd533b5882268a85ca9aacae81628332d4ff
Tristan Bendixen <angelod1981@gmail.com>
2018-10-29 13:50:48 +0100

    Formatting inputAssignment according to PEP8.

--- python-assignments/inputAssignment.py
+++ python-assignments/inputAssignment.py
@@ -10,11 +10,13 @@ try:
 except ValueError:
     print("You did not enter a number")
     sys.exit(0)
-print('You will be 100 years old in {}'.format(datetime.datetime.now().year-age+100))
+print('You will be 100 years old in {}'.format(
+    datetime.datetime.now().year-age+100))
 print("Would you like to have the last massage printed again? (yes/no)")
 con = input()
 if(con.upper() == "YES" or con.upper() == "Y"):
     print("How many times?")
     n = int(input())
-    for i in range(0,n-1):
-        print('{}. You will be 100 years old in {}'.format(i, datetime.datetime.now().year-age+100))
+    for i in range(0, n-1):
+        print('{}. You will be 100 years old in {}'.format(
+            i, datetime.datetime.now().year-age+100))
